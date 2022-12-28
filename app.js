98031ddf72445053e0db3ff75d01152782849e43
isturiz <isturizmauricio@gmail.com>
2022-12-28 00:31:15 -0400

    add: functionality of the prev and next buttons

--- app.js
+++ app.js
@@ -2,24 +2,19 @@
 //   return Math.floor(Math.random() * (max - min)) + min;
 // }
 
+let pokemon = {}
 
-document.addEventListener('keyup', () => {
-// document.addEventListener('DOMContentLoaded', () => {
-
-
-  const pokemonName = document.getElementById('pokemon-name__input').value.toLowerCase();
-  fetchData(pokemonName)
-})
+const updateName = (name) => {
+  document.getElementById('pokemon-name__input').value = `${name}`
+}
 
 const fetchData = async (pokemonName) => {
 
-  console.log(pokemonName);
-  
   try {
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     const data = await res.json()
 
-    const pokemon = {
+    pokemon = {
       img: data.sprites.front_default,
       name: data.name,
       id: data.id,
@@ -29,25 +24,48 @@ const fetchData = async (pokemonName) => {
       speed: data.stats[5].base_stat
     }
 
+    setTimeout(updateName(pokemon.name), 5000)
+
     drawCard(pokemon)
 
   } catch (error) {
     console.log(error);
-    console.log('nombre o id no válidos');
-
   }
 }
 
+document.addEventListener('keyup', () => {
+// document.addEventListener('DOMContentLoaded', () => {
+  const pokemonName = document.getElementById('pokemon-name__input').value.toLowerCase();
+  fetchData(pokemonName)
+})
+
+
+let prev = document.querySelector('#prev')
+let next = document.querySelector('#next')
+
+prev.addEventListener('click', () => {
+  const pokePrev = pokemon.id - 1
+  console.log(pokePrev);
+  fetchData(pokePrev)
+
+  
+
+})
+
+next.addEventListener('click', () => {
+  const pokeNext = pokemon.id + 1
+  console.log(pokeNext);
+  fetchData(pokeNext)
+})
+
 const drawCard = (pokemon) => {
-  console.log(pokemon);
 
-  const flex = document.querySelector('.flex') 
-  const template = document.querySelector('.card')
-  const clone = template.cloneNode(true)
-  const fragment = document.createDocumentFragment()
+  const card = document.querySelector('.card')
 
-  template.querySelector('.card-body__img').setAttribute('src', pokemon.img)
-  template.querySelector('.card-body__title').innerHTML = `${pokemon.name} <span>N.º${pokemon.id}</span>`
+
+  card.querySelector('.card-body__img').setAttribute('src', pokemon.img)
+  card.querySelector('.card-body__title').innerHTML = `${pokemon.name} <span>N.º${pokemon.id}</span>`
+  card.querySelector('.card-type').innerHTML = `${pokemon.attack}`
   // clone.querySelector('.card-body-text').innerHTML = `${pokemon.type}`
 
   // clone.querySelectorAll('.card-footer h3')[0].innerHTML = `${pokemon.attack}`
